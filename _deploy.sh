519523e0fda437f1d262dc8781b02d154e13c0c4
Brooks Ambrose <brooksambrose@berkeley.edu>
2017-07-21 15:12:36 -0700

    remove global flag

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so people know who your commits belong to!
-# git config --global user.email "calnet@berkeley.edu"
-# git config --global user.name "Oski Bear"
+# git config user.email "calnet@berkeley.edu"
+# git config user.name "Oski Bear"
 
 # make sure you're on the master branch
 git checkout master
