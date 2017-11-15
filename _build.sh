9cf8428e8828d9ee580f5708a17c16e15a7445a7
Brooks Ambrose <brooksambrose@gmail.com>
2017-11-15 15:54:45 -0800

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-pkill caddy
 cd /home/rstudio/ && rm -rf _book/ _main*md \
-&& Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' \
-&& cd /home/rstudio/_book && caddy
+&& Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
