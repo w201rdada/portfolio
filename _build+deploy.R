bfbb8babdf00d58a40ab2947e4119c8d0799a70b
Brooks Ambrose <brooksambrose@gmail.com>
2017-11-16 21:35:19 -0800

    Update _build+deploy.R

--- _build+deploy.R
+++ _build+deploy.R
@@ -1,6 +1,8 @@
 # BUILD # Run the build line to render .Rmd to .html, and to serve the preview website on the preview port
 
-system("bash _build.sh",wait=F)
+system("bash _build.sh",wait=T)
+viewer <- getOption("viewer")
+viewer('/home/rstudio/_book/index.html') # replace 'index.html' with whichever page you're working on
 
 # DEPLOY # Always after building, run the deploy line to copy .html from the preview folder to the deployment folder (/home/rstudio/docs/), commit all changes, add a robots.txt, and prepare for manual push to origin. Even after running the script your site will NOT be deployed until you push.
 
